5168599582a9437a3df1b38d8d77a2820950098e
jkurambhatti <jkurambhatti2992@gmail.com>
2016-08-25 20:43:08 +0530

    created routes for INSERT, LIST, DELETE, UPDATE

---
+++ handlers.go
@@ -0,0 +1,1 @@
+package todoapp_go
---
+++ routes.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Route struct {
+	Method      string
+	Path        string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = []Route{
+	Route{
+		Method:      "GET",
+		Path:        "/",
+		HandlerFunc: http.HandlerFunc(Index)},
+	Route{
+		Method:      "POST",
+		Path:        "/insert",
+		HandlerFunc: http.HandlerFunc(CreateTodo)},
+	Route{
+		Method:      "DELETE",
+		Path:        "/list/{id}",
+		HandlerFunc: http.HandlerFunc(DeleteTodo)},
+	Route{
+		Method:      "GET",
+		Path:        "/list",
+		HandlerFunc: http.HandlerFunc(ShowTodo)},
+	Route{
+		Method:      "PUT",
+		Path:        "/list/{id}",
+		HandlerFunc: http.HandlerFunc(UpdateTodo)},
+}
+
+func NewRouter() *mux.Router {
+// StrictSlash defines the trailing slash behavior for new routes. The initial value is false.
+// When true, if the route path is "/path/", accessing "/path" will redirect to the former and vice versa.
+// When false, if the route path is "/path", accessing "/path/" will not match this route and vice versa.
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes {
+		router.Methods(route.Method).
+			Path(route.Path).
+			HandlerFunc(route.HandlerFunc)
+	}
+	return router
+}
